ebbc25d23becfaae06f0fbe914b6193c3a69c654
Shamed Calderon <arzu.calderon@gmail.com>
2023-04-26 17:57:52 -0500

    feature: add widget form to store a new repository

--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -1,3 +1,6 @@
+import { useMemo } from "react";
+
+import { AddWidgetForm } from "../../components/AddWidgetForm/AddWidgetForm";
 import { GithubRepositoryWidget } from "../../components/GithubRepositoryWidget/GithubRepositoryWidget";
 import { GithubRepositoryWidgetSkeleton } from "../../components/GithubRepositoryWidget/GithubRepositoryWidgetSkeleton";
 import { config } from "../../config/devdash";
@@ -5,32 +8,33 @@ import { useGithubRepositories } from "../../hooks/useGithubRepositories";
 import { ApiGithubRepository } from "../../interfaces/ApiGithubRepository";
 import styles from "./Dashboard.module.scss";
 
-const githubRepositoryUrls = config.widgets.map((widget) => widget.repository_url);
-
 export function Dashboard({ repository }: { repository: ApiGithubRepository }) {
+	const githubRepositoryUrls = useMemo(() => {
+		return config.widgets.map((widget) => widget.repository_url);
+	}, []);
+
 	const { repositoryData, isLoading } = useGithubRepositories(repository, githubRepositoryUrls);
 
 	return (
 		<>
-			{isLoading && (
-				<section className={styles.container}>
+			<section className={styles.container}>
+				{isLoading ? (
 					<GithubRepositoryWidgetSkeleton numberOfWidgets={githubRepositoryUrls.length} />
-				</section>
-			)}
-
-			{!isLoading && repositoryData.length === 0 ? (
-				<div className={styles.empty}>
-					<span>No hay widgets configurados</span>
-				</div>
-			) : (
-				<section className={styles.container}>
-					{repositoryData.map((repository) => (
+				) : (
+					repositoryData.map((repository) => (
 						<GithubRepositoryWidget
 							key={`${repository.id.organization}/${repository.id.name}`}
 							widget={repository}
 						/>
-					))}
-				</section>
+					))
+				)}
+				<AddWidgetForm />
+			</section>
+
+			{isLoading && repositoryData.length === 0 && (
+				<div className={styles.empty}>
+					<span>No hay widgets configurados</span>
+				</div>
 			)}
 		</>
 	);
